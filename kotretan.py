1e1b6202aa3eea2a71819b37dbd89d61c2ebbe37
IMMPuteraKanu <101811528+IMMPuteraKanu@users.noreply.github.com>
2023-06-16 15:15:17 +0700

    kotretan.py

--- kotretan.py
+++ kotretan.py
@@ -5,7 +5,7 @@ import altair as alt
 # Dataset
 df = pd.read_csv("https://raw.githubusercontent.com/IMMPuteraKanu/Visdat/main/IndonesianSalary.csv")
 
-# Set up Streamlit components
+# Streamlit Button 
 st.title('Dataset Visualization')
 st.subheader('Select Options')
 selected_area1 = st.selectbox("Region 1", df['REGION'].unique())
@@ -13,20 +13,24 @@ selected_area2 = st.selectbox("Region 2", df['REGION'].unique())
 start_year = st.slider("From", min_value=df['YEAR'].min(), max_value=df['YEAR'].max(), value=df['YEAR'].min())
 end_year = st.slider("To", min_value=df['YEAR'].min(), max_value=df['YEAR'].max(), value=df['YEAR'].max())
 
-# Filter the data for region 1
+# Filter Data Region 1
 filtered_data1 = df[(df['REGION'] == selected_area1) & (df['YEAR'] >= start_year) & (df['YEAR'] <= end_year)]
 
-# Filter the data for region 2
+# Filter Data Region 2
 filtered_data2 = df[(df['REGION'] == selected_area2) & (df['YEAR'] >= start_year) & (df['YEAR'] <= end_year)]
 
 # Combine the data for both regions
 combined_data = pd.concat([filtered_data1, filtered_data2])
 
+# Calculate the average salary difference
+average_diff = filtered_data1['SALARY'].mean() - filtered_data2['SALARY'].mean()
+
 # Create the visualization
 chart = alt.Chart(combined_data).mark_line().encode(
     x='YEAR',
     y='SALARY',
-    color='REGION'
+    color='REGION',
+    tooltip=['YEAR', 'SALARY']
 ).properties(
     width=600,
     height=400,
@@ -35,3 +39,7 @@ chart = alt.Chart(combined_data).mark_line().encode(
 
 # Display Grafik
 st.altair_chart(chart)
+
+# Display the average salary difference
+st.subheader('Average Salary Difference:')
+st.write("The average salary difference between {} and {} is: {:.2f}".format(selected_area1, selected_area2, average_diff))
